692bd0a17cd0432a48d60a1d61ef2219570c56c7
RichoAlbertTio <study160702@gmail.com>
2025-08-05 21:00:54 +0700

    Layouting Pages Router Part 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import RootLayout from "@/layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  // push, back, query, reload, replace
+  // console.log('router =>', router);
+
+  const metaTitle = router.pathname === '/' ? 'home' : router.pathname.replace('/', '');
   return (
-    // title tidak wajihb diisi, karena tidak sudah di handle di layout/index.tsx
-    <RootLayout >
+    // title tidak wajib diisi, karena tidak sudah di handle di layout/index.tsx
+    <RootLayout metaTitle={metaTitle}>
       <Component {...pageProps} />
     </RootLayout>
   );
