e655f702e7fc11fe6413d70ff713d137fb1bd52a
RichoAlbertTio <study160702@gmail.com>
2025-08-06 03:27:33 +0700

    Styling TailwindCSS Pages Router Part 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
     <>
-       <div>
+      {/* <div>
         Index Page
-       </div>
+       </div> */}
+
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      {/* <a href="" className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300">Click me</a> */}
     </>
   );
 }
